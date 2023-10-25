a5017f607a6decbfdcd03a46654d1e37037c9342
galleta <yampara.joel.794@gmail.com>
2023-10-25 03:27:59 -0400

    modificacion reporte.php

--- application/controllers/reporte.php
+++ application/controllers/reporte.php
@@ -10,25 +10,12 @@ class Reporte extends CI_Controller {
 			$tipo = $this->session->userdata('rol');
 			if($tipo == 'administrador')
 			{	
-				$lista = $this->venta_model->listaVentas();
-				$data['venta']=$lista;
 
 				$this->load->view('administrador/cuerpoLte/cabecera');
 				$this->load->view('administrador/cuerpoLte/menuSuperior');
 				$this->load->view('administrador/cuerpoLte/menuLateral');
-				$this->load->view('administrador/vistaReporte/lista_reporte',$data);//vista de la categoria
+				$this->load->view('administrador/vistaReporte/lista_reporte');//vista de la categoria
 				$this->load->view('administrador/cuerpoLte/pie');
-			}
-			else
-			{
-				$lista=$this->venta_model->listaVentas();
-				$data['venta']=$lista;
-
-				$this->load->view('empleado/cuerpoLte/cabecera');
-				$this->load->view('empleado/cuerpoLte/menuSuperior');
-				$this->load->view('empleado/cuerpoLte/menuLateral');
-				$this->load->view('empleado/vistaVenta/venta_lista',$data);//vista de la categoria
-				$this->load->view('empleado/cuerpoLte/pie');
 			}
 		}
 		else{
