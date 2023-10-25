19524411db97a362350034518109c5e3918e103f
galleta <yampara.joel.794@gmail.com>
2023-10-25 01:26:44 -0400

    adicion encriptar

---
+++ encriptar.php
@@ -0,0 +1,11 @@
+<?php
+    $clave='web2023';
+    $clave2='sis2023';
+    echo 'contraseña 1 '.md5($clave);
+    echo '<br>';
+    echo 'contraseña 2 '.md5($clave2);
+
+    //admin 497a0577749efcdab301d8418c8e1670 web2023    
+    //empleado 16bd179b42059f99f90d6a23453154c0 sis2023
+
+?>
