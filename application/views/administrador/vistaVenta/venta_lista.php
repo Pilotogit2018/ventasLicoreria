61edece34d6f42a4e62ef85c085ae88bc093427f
galleta <yampara.joel.794@gmail.com>
2023-10-25 02:02:37 -0400

    eliminacion de deslizador

--- application/views/administrador/vistaVenta/venta_lista.php
+++ application/views/administrador/vistaVenta/venta_lista.php
@@ -8,8 +8,7 @@
                   <div class="x_title">
                     <h2>Lista de las Ventas</h2>
                     <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
+                      
                       <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
                         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
