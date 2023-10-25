6fcdb0c2a2217e68053992133c04a76e489480d0
galleta <yampara.joel.794@gmail.com>
2023-10-25 02:53:34 -0400

    modificacion en controlador reporte

--- application/controllers/reporte.php
+++ application/controllers/reporte.php
@@ -5,7 +5,36 @@ class Proveedor extends CI_Controller {
    
     public function lista()
 	{
-		
+        if($this->session->userdata('login'))
+		{
+			$tipo = $this->session->userdata('rol');
+			if($tipo == 'administrador')
+			{	
+				$lista = $this->venta_model->listaVentas();
+				$data['venta']=$lista;
+
+				$this->load->view('administrador/cuerpoLte/cabecera');
+				$this->load->view('administrador/cuerpoLte/menuSuperior');
+				$this->load->view('administrador/cuerpoLte/menuLateral');
+				$this->load->view('administrador/vistaVenta/venta_lista',$data);//vista de la categoria
+				$this->load->view('administrador/cuerpoLte/pie');
+			}
+			else
+			{
+				$lista=$this->venta_model->listaVentas();
+				$data['venta']=$lista;
+
+				$this->load->view('empleado/cuerpoLte/cabecera');
+				$this->load->view('empleado/cuerpoLte/menuSuperior');
+				$this->load->view('empleado/cuerpoLte/menuLateral');
+				$this->load->view('empleado/vistaVenta/venta_lista',$data);//vista de la categoria
+				$this->load->view('empleado/cuerpoLte/pie');
+			}
+		}
+		else{
+			$data['mensaje']=$this->uri->segment(3);//
+			$this->load->view('logins/login',$data);//vista del login
+		}
     }
  
 
