5c3eb73663a3003c7e6d23ad742be0975bf8ef52
galleta <yampara.joel.794@gmail.com>
2023-10-25 03:28:40 -0400

    modificacion lista

---
+++ application/views/administrador/vistaReporte/lista_reporte.php
@@ -0,0 +1,39 @@
+
+<div class="right_col" role="main">
+    <div class="">
+        <div class="row">
+        <div class="col-md-12 col-sm-12 ">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Lista de Proveedores</h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="<?php echo base_url();?>index.php/proveedor/agregarProvLte">Agregar proveedor</a>
+                            <a class="dropdown-item" href="<?php echo base_url();?>index.php/proveedor/deshabilitados">deshabilitados</a>
+                          </div>
+                      </li>
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                      <div class="row">
+                          <div class="col-sm-12">
+                            <div class="card-box table-responsive">
+                    <p class="text-muted font-13 m-b-30">
+                      The Buttons extension for DataTables provides a common set of options, API methods and styling to display buttons on a page that will interact with a DataTable. The core library provides the based framework upon which plug-ins can built.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+                </div>
+              </div>
+        </div>
+    </div>
+</div>
